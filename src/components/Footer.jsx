96f7fe75983cfc17f3f4f27ccd81ce3b471ffefe
Abhishek Kumar Singh <109914650+abhishek00089@users.noreply.github.com>
2024-04-15 11:40:36 +0530

    First commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h1>Morning-Star</h1>
+                <p>@all rights reserved</p>
+            </div>
+
+            <div>
+                <h5>Follow Us</h5>
+                <div>
+                    <a href="https://youtube.com" target='blank'>Youtube</a>
+                    <a href="https://www.linkedin.com/in/abhishek-kumar-singh-58a513194/" target='blank'>LinkedIn</a>
+                    <a href="https://github.com/abhishek00089" target='blank'>Github</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
